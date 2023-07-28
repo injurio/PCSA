ad59c100dc7edc65958a0cf332e033fee8d543ec
injurio <49044584+injurio@users.noreply.github.com>
2023-07-28 09:48:45 +0700

    IC shell 0.2.0

--- IC shell/icsh.c
+++ IC shell/icsh.c
@@ -10,13 +10,20 @@
 
 #define MAX_CMD_BUFFER 255
 
-char *res;
-char wow[];
-void command(char *);
-void my_history(char *);
+int i;
+char *res, last_command[MAX_CMD_BUFFER];
+void command(char *), my_history(char *), my_read(char *);
 
-int main() {
+int main(int argc, char * argv []) {
 	char buffer[MAX_CMD_BUFFER];
+	// char * input = argv[1];
+    // for (i = 1; i < argc; i++)
+    // {
+    //     printf("%s ", argv[i]);       
+    // }
+	if (argv[1] != NULL) {
+		my_read(argv[1]);
+	}
 	while(1) {
 		printf("icsh $ ");
         fgets(buffer, 255, stdin);
@@ -33,8 +40,8 @@ void command(char *buffer) {
         printf("%s", res);
     } 
     else if (strstr(buffer, "!!")){
-		strcpy(buffer, wow);
-		printf("%s", buffer);
+		strcpy(buffer, last_command);
+		// printf("%s", buffer);
 		command(buffer);
     } 
     else if (strstr(buffer, "exit")) {
@@ -42,7 +49,7 @@ void command(char *buffer) {
             ;
         if (*res) res++;
         if (atoi(res) && atoi(res) >= 1) {
-            printf("bye\n");
+            // printf("bye\n");
             exit(atoi(res));
         } else {
             printf("bad command\n");
@@ -53,5 +60,16 @@ void command(char *buffer) {
 }
 
 void my_history(char *buffer){
-	strcpy(wow, buffer);
+	strcpy(last_command, buffer);
+}
+
+void my_read(char *filename) {
+	FILE* ptr;
+    ptr = fopen(filename, "r");
+ 
+	char line[500];
+	while (fgets(line, sizeof(line), ptr)) {
+		command(line);
+	}
+    fclose(ptr);
 }
